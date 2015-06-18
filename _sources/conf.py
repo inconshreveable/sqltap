fc92f36d9d65d6bac5ed85e555b26873a9adf39e
Alan Shreve <alan@inconshreveable.com>
2015-06-18 00:24:41 -0700

    generate docs for latest version

---
+++ _sources/conf.py
@@ -0,0 +1,11 @@
+master_doc = "index"
+source_suffix = ".txt"
+extensions = ["sphinx.ext.autodoc"]
+html_title = "sqltap 0.3 documentation"
+html_style = "nature.css"
+copyright = "2015, inconshreveable"
+project = "sqltap"
+release = "0.3"
+html_theme_options = {
+    "nosidebar": True
+}
